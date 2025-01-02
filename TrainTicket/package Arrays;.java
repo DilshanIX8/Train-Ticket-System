4879ac261e1d4e10a7572563deaaab982f138974
DILSHAN-IX8 <dilshanvitharana18@gmail.com>
2025-01-02 18:02:31 +0530

    Add files via upload

---
+++ TrainTicket/package Arrays;.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class Arrays{
+
+    public static void main(String [] arg){
+
+        int arr[] = {0,1,2,3,4,};
+
+        System.out.println(+arr[0]);
+        System.out.println("/n" +arr[1]);
+        System.out.println("/n" +arr[2]);
+        System.out.println("/n" +arr[3]);
+        System.out.println("/n" +arr[4]);
+    }
+    
+}
